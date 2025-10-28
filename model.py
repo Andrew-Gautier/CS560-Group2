b7ed23e998fd22a9a75f129e3190a5e67be013d0
aeg00011 <andrewgautier777@gmail.com>
2025-10-28 11:25:53 -0400

    lstm model skeleton

---
+++ model.py
@@ -0,0 +1,22 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import pandas as pd
+import numpy as np
+
+### Time Series Classifier for Reddit Comment sentiment used to predict stock market direction
+
+
+class LSTMClassifier(nn.Module):
+    def __init__(self):
+        super().__init__()
+
+
+def grid_parameter_tune():
+    return
+def train_model():
+    return
+def evaluate_model():
+    return
+def test_model():
+    return
