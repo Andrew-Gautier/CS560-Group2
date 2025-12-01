fc91980761fd2f1a9eb86cf8016b6289394f97af
aeg00011 <andrewgautier777@gmail.com>
2025-12-01 15:36:55 -0500

    cleanup old uneeded files + data

--- fine-tuning.py
+++
@@ -1,5 +0,0 @@
-from transformers import AutoTokenizer, AutoModel
-
-# Start with FinBERT
-tokenizer = AutoTokenizer.from_pretrained("ProsusAI/finbert")
-model = AutoModel.from_pretrained("ProsusAI/finbert")
